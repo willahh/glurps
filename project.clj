090f1cf23032e9ea4ece9306dd7e317f078ed0ed
William Ravel <wravel@gmail.com>
2018-03-31 21:12:29 +0200

    Update

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [hiccup "1.0.5"]
+                 [hickory "0.7.1"]
+                 [enlive "1.1.6"]]
   :main ^:skip-aot glurps.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/glurps/core.clj
+++ src/glurps/core.clj
@@ -1,12 +1,64 @@
 (ns glurps.core
-  (:gen-class))
+  (:require [hiccup.page :refer [include-js include-css html5]]
+            [hickory.core]
+            [net.cgrand.enlive-html :as ehtml]
+            [clojure.string :as str]))
 
 (defn parse-url [url]
   (println url))
 
-(def html (slurp "https://news.ycombinator.com/"))
+;; ;; (def html (slurp "https://news.ycombinator.com/"))
+;; ;; (def tmlh (slurp "resources/new.ycombinator.com.html"))
+;; (def html (slurp "http://www.allocine.fr/"))
+;; (def html-hiccup (hickory.core/as-hiccup (hickory.core/parse html)))
 
-html
+;; (ehtml/select (ehtml/text html) [:a])
+;; (ehtml/at [:ul] [:a :b])
+;; (ehtml/select html-hiccup [:body])
+
+
+(def url "http://www.allocine.fr/")
+
+(defn fetch-url [url]
+  (ehtml/html-resource (java.net.URL. url)))
+
+(def page (fetch-url url))
+
+
+(defn get-sorties-week []
+  (let [rows  (map ehtml/text (ehtml/select page [:div.mdl-inside :div.roller-item :div.meta]))]
+    rows))
+
+(defn clean-string [str]
+  "Removes excess spaces at the beginning and end of the chain, as well as line
+breaks"
+  (clojure.string/replace
+   (clojure.string/replace (clojure.string/replace str #"^ +" "")
+                           #" +$" "") #"\n" ""))
+
+(defn get-sorties-week-author [str]
+  (clojure.string/replace
+   (clean-string (nth (clojure.string/split str #" \n") 2))
+   #"^De " ""))
+
+(defn get-sorties-week-title [str]
+  (clean-string (nth (clojure.string/split str #" \n") 0)))
+
+(get-sorties-week-author (nth (get-sorties-week) 1))
+(get-sorties-week-title (nth (get-sorties-week) 1))
+
+(defn get-sorties-week-rows []
+  (map (fn [row]
+         {:author (get-sorties-week-author row)
+          :title (get-sorties-week-title row)})
+       (get-sorties-week)))
+
+(get-sorties-week-rows)
+
+
+
+
+
 
 
 (defn -main [& args]
