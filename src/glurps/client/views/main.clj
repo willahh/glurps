85b8d27edb68cdbcad663e7b2755066e36f269fa
William Ravel <wravel@gmail.com>
2018-04-01 06:38:45 +0200

    Update

--- src/glurps/client/views/main.clj
+++ src/glurps/client/views/main.clj
@@ -3,48 +3,58 @@
             [hiccup.core :as core]
             [glurps.admin.home]))
 
+(def main-nav-rows [{:label "Home" :href "/"}
+                    {:label "Week" :href "/week"}])
+
 (defn get-head []
   [:head
    [:title "Glurps!"]
-   (page/include-css "/lib/bootstrap-4.0.0-beta-dist/css/bootstrap.min.css")
-   (page/include-js "/lib/jquery-3.2.1/jquery-3.2.1.min.js") 
-   (page/include-js "/lib/popper/popper.min.js") 
-   (page/include-js "/lib/bootstrap-4.0.0-beta-dist/js/bootstrap.min.js") 
+   (page/include-css "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css")
+   (page/include-css "https://code.jquery.com/jquery-3.2.1.slim.min.js")
+   (page/include-css "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js")
+   (page/include-css "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js")
    (page/include-css "/css/styles.css")])
 
-(defn get-header []
+(defn get-main-nav-view [main-nav-rows]
+  [:ul {:class "navbar-nav mr-auto"}
+   (map (fn [row]
+          [:li {:class "nav-item active"}
+           [:a.nav-link {:href (row :href)} (row :label)]]) main-nav-rows)])
+
+(defn get-page-header []
   [:nav {:class "navbar navbar-expand-lg navbar-light bg-light"}
    [:a.navbar-brand {:href "#"}]
    [:button.navbar-toggler {:type "button" :data-toggle "collapse" :data-target "#navbarSupportedContent"}
     [:span.navbar-toggler-icon]]
    [:div#navbarSupportedContent {:class "collapse navbar-collapse"}
-    [:ul {:class "navbar-nav mr-auto"}
-     [:li {:class "nav-item active"}
-      [:a.nav-link {:href "/"} "Home" [:span.sr-only "(current)"]]]
-     [:li {:class "nav-item"}
-      [:a.nav-link {:href "/add-location"} "Add location"]]
-     [:li {:class "nav-item"}
-      [:a {:class "nav-link" :href "/all-locations"} "All locations"]]
-     ]
+    (get-main-nav-view main-nav-rows)
     [:form {:class "form-inline my-2 my-lg-0"}
      [:input {:class "form-control mr-sm-2" :type "text" :placeholder "Search"}]
      [:button {:class "btn btn-outline-succes my-2 my-sm-0" :type "submit"} "Submit"]]]])
 
 (defn get-html [hiccup-html]
   (page/html5 (get-head)
-              [:body (get-header) hiccup-html]))
+              [:body {:class "bg-light"}
+               [:div.container (get-page-header) hiccup-html]]))
 
 (defn get-item-view [row]
-  [:div [:h1 (row :title)]
-   [:p.description (row :description)]
-   [:p.date (row :date)]
-   [:p.director (row :director)]
-   [:p.genre (row :genre)]])
+  [:div {:class "col-md-4 mb-4"}
+   [:div {:class "card" :style "height: 100%"}
+    [:img.card-img-top ]
+    [:div.card-body
+     [:h5.card-title (row :title)]
+     [:div.card-text
+      [:div (row :description)
+       [:p.date (row :date)]
+       [:p.director (row :director)]
+       [:p.genre (row :genre)]]]
+     [:a {:class "btn btn-primary"} "Go"]]]])
 
 (defn get-item-list-view [rows]
   (map #(get-item-view %) rows))
 
 (defn get-root-html []
-  (get-html (get-item-list-view (glurps.admin.home/get-sorties-week-rows-debug))))
+  (get-html [:div.row
+             (get-item-list-view (glurps.admin.home/get-sorties-week-rows-debug))]))
 
 
