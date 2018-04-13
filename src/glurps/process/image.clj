c0c602fa8d224f30a95c12e145c15287628260f4
William Ravel <wravel@gmail.com>
2018-04-14 00:08:32 +0200

    Update

---
+++ src/glurps/process/image.clj
@@ -0,0 +1,11 @@
+(ns glurps.process.image
+  (:require [clj-http.client :as client]))
+
+
+(defn get-dir []
+  "upload/")
+
+(defn extract-image-from-url [url path]
+  (clojure.java.io/copy
+   (:body (client/get url {:as :stream}))
+   (java.io.File. (str (get-dir) path))))
