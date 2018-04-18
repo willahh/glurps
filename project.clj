d418af163fca619e9f87782491ed344fd995b946
William Ravel <wravel@gmail.com>
2018-04-19 01:08:04 +0200

    Update

--- project.clj
+++ project.clj
@@ -4,25 +4,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.cache "0.7.1"]
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [org.clojure/test.check "0.9.0"]
+                 [org.clojure/test.generative "0.5.2"]
+                 [ring "1.6.3"]
+                 [ring/ring-defaults "0.3.1"]
+                 [clj-http "3.8.0"]
+                 [compojure "1.6.0"]
+                 [sqlitejdbc "0.5.6"]
                  [hiccup "1.0.5"]
                  [hickory "0.7.1"]
                  [enlive "1.1.6"]
-                 [org.clojure/core.cache "0.7.1"]
-                 [compojure "1.6.0"]
-                 [ring "1.6.3"]
-                 [org.clojure/java.jdbc "0.7.5"]
-                 [sqlitejdbc "0.5.6"]
-                 [org.clojure/test.check "0.9.0"]
-                 [org.clojure/test.generative "0.5.2"]
-                 [prone "1.5.1"]
-                 [clj-http "3.8.0"]                 
-                 [ring/ring-defaults "0.3.1"]]
+                 [prone "1.5.1"]]
   :main ^:skip-aot glurps.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:ring {:stacktrace-middleware prone.middleware/wrap-exceptions}
                    :dependencies [[org.clojure/test.check "0.9.0"]]}}
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler glurps.client.routes/app
-         :init glurps.client.routes/init})
-
+  :ring {:handler glurps.core})
--- src/glurps/core.clj
+++ src/glurps/core.clj
@@ -1,1 +1,49 @@
-(ns glurps.core)
+(ns glurps.core
+  (:use compojure.core)
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [compojure.core :as compojure]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [glurps.admin.index :as admin-list]
+            [glurps.admin.actor.route :refer [admin-actor-route]]
+            [glurps.client.views.home :as home]
+            [glurps.client.views.sheet :as sheet]
+            [glurps.client.views.week :as week]
+            [glurps.client.views.logs :as logs]
+            [glurps.client.views.actor :as actor]))
+
+(use 'ring.middleware.resource
+     'ring.middleware.content-type
+     'ring.middleware.not-modified
+     'ring.middleware.params
+     'ring.middleware.keyword-params)
+
+(defroutes app-routes
+  (GET "/"
+       []
+       (home/get-html))
+  (GET "/week"
+       []
+       (week/get-html))
+  (GET "/sheet/:id"
+       [id]
+       (sheet/get-html id))
+  (GET "/actor"
+       []
+       (actor/get-html))
+  (POST "/actor"
+        [params] (prn-str params))
+  (GET "/logs"
+       []
+       (logs/get-html))
+  (GET "/admin"
+       []
+       (admin-list/get-html)))
+
+(def app
+  (-> (routes app-routes admin-actor-route)
+      (wrap-resource "public")
+      (wrap-keyword-params)
+      (wrap-params)
+      (wrap-content-type)
+      (wrap-not-modified)))
