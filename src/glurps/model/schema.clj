a8c70814e033a488afd3b022cdc1e542fcd5b8ff
William Ravel <wravel@gmail.com>
2018-05-23 23:18:42 +0200

    Update

--- src/glurps/model/schema.clj
+++ src/glurps/model/schema.clj
@@ -2,103 +2,67 @@
   (:use korma.db
         korma.core)
   (:require [clojure.java.jdbc :as j]
-            [clojure.spec.alpha :as s]))
-
-(defn merge-vector-of-map [v1 v2]
-  "Merge two vectors of map.
-  Note: this is bad implemented but i don't know how
-  do better for now."
-  (let [cnt (count v2)
-        result (atom [])]
-    (dotimes [i cnt]
-      (let [m1 (when (< i (count v1)) (nth v1 i))
-            m2 (nth v2 i)]
-        (swap! result conj (merge m1 m2))))
-    @result))
-
-(defn create-table [table]
-  (let [name (:name table)
-        columns (:jdbc-columns table)]
-    (j/db-do-commands db 
-                      (clojure.string/lower-case 
-                       (j/create-table-ddl name columns
-                                           {:table-spec "ENGINE=InnoDB"
-                                            :entities clojure.string/upper-case})))))
-
-(defn drop-table [name]
-  (j/db-do-commands db (j/drop-table-ddl name)))
-
-(defn map-column-to-jdbc-column [col]
-  (into []
-        (flatten
-         (map (fn [a]
-                (let [k (first a)
-                      v (first (rest a))]
-                  (conj []
-                        (case k
-                          :name [(keyword v)]
-                          :type [v]
-                          :null (if-not v [:not :null])
-                          :primary (when v [:primary :key])
-                          :auto_increment (when v [:auto_increment])
-                          :default (when v [:default v])
-                          nil)))) col))))
-
-(defn def-glu-table [name columns & {:keys [extend]}]
-  (let [abstract-columns (when extend (:columns extend))
-        abstract-jdbc-columns (when abstract-columns
-                                (map map-column-to-jdbc-column abstract-columns))
-        columns (merge-vector-of-map abstract-columns columns)
-        target-jdbc-columns (map map-column-to-jdbc-column columns)        
-        jdbc-columns (into []
-                           (if abstract-jdbc-columns 
-                             (apply merge abstract-jdbc-columns target-jdbc-columns)
-                             target-jdbc-columns))] 
-    
-    {:name name
-     :columns columns
-     :jdbc-columns jdbc-columns
-     :fields (into [] (map #(first %) jdbc-columns))}))
-
-(defn map-columns-to-entity-fields [glu-table]
-  (map #(keyword (:name %)) (:columns glu-table)))
-
-
-
-;; -------------------------------------------------
-;; Define the db
-(def db (mysql {:host "localhost"
-                :db "glurps"
-                :user "root"
-                :password "root"}))
-
-(defdb glurps db)
+            [clojure.spec.alpha :as s]
+            [glurps.process.db.db :as db]))
 
 
 ;; -- Define tables -----------------------------------------------
 (def abstract-col-table
-  (def-glu-table "glu_abstract_col" [{:name "create_date" :type "datetime" :null false}
-                                     {:name "update_date" :type "datetime" :null false}
-                                     {:name "active" :type "boolean" :default "1"}
-                                     {:name "fav" :type "boolean" :default "0"}]))
+  (db/def-glu-table "glu_abstract_col" [{:name "create_date" :type "datetime" :null false}
+                                        {:name "update_date" :type "datetime" :null false}
+                                        {:name "active" :type "boolean" :default "1"}
+                                        {:name "fav" :type "boolean" :default "0"}]))
 
 (def group-table 
-  (def-glu-table "glu_group" [{:name "group_id" :type "int" :null false :primary true :auto_increment true}
-                              {:name "name" :type "varchar(255)" :null false}]
+  (db/def-glu-table "glu_group" [{:name "group_id" :type "int" :null false :primary true :auto_increment true}
+                                 {:name "name" :type "varchar(255)" :null false}]
     :extend abstract-col-table))
 
-
+(def user-table 
+  (db/def-glu-table "glu_user" [{:name "user_id" :type "int" :null false :primary true :auto_increment true}
+                                {:name "name" :type "varchar(255)" :null false}
+                                {:name "create_date" :type "datetime" :null false}
+                                {:name "update_date" :type "datetime" :null false}
+                                {:name "active" :type "boolean" :default "1"}]))
 
 ;; -- Define entities -----------------------------------------------
 (defentity glu-group
   (table :glu_group)
-  (database glurps)
+  (database db/glurps)
   (entity-fields (:fields group-table)))
 
-;; Create tables
-(create-table group-table)
+(defentity glu-group
+  (table :glu_group)
+  (database db/glurps)
+  (entity-fields :name :group_id :create_date :update_date :active :fav))
+
+;; (defentity glu-group
+;;   (table :glu_group)
+;;   (database glurps)
+;;   (entity-fields `@~(into () (:fields group-table))))
 
 
+
+
+
+
+
+
+;; Create tables ----------------------------
+(db/create-table db/db user-table)
+(db/create-table db/db group-table)
+
+
+
+
+
+
+
+
+
+
+
+;; Test some data insert / select
 ;; Define some specs
 ;; (s/def ::group_id int?)
 ;; (s/def ::name string?)
@@ -114,25 +78,25 @@
 ;; (defrecord Group [name create_date update_date])
 ;; (s/explain-str ::group (->Group "yo" (new java.util.Date) (new java.util.Date)))
 
-
-
-
-;; Do some inset
-(insert glu-group (values {:name "test"
+;; Do some insert ----------
+(insert glu-group (values {:name (str "test" (rand)) 
                            :create_date (new java.util.Date)
                            :update_date (new java.util.Date)}))
 
-;; Do some select
+;; Some insert select
+(db/select-from-params glu-group {})
+(db/select-from-params glu-group {:fav "on"})
+(db/select-from-params glu-group {:fav "on" :active "on" :order "group_id" :asc "0" :limit 2 :offset 0})
+(db/select-from-params glu-group {:fav "on" :active "on" :order "name"})
+(db/select-from-params glu-group {:fav "on" :active "on" :order "group_id" :asc "1"})
+(db/select-from-params glu-group {:fav "on" :active "on" :order "name" :asc "1" :limit 2 :offset 1})
 (select glu-group)
+(select glu-group
+        (where {:active true
+                :fav "on"})
+        (order :name :DESC)
+        (limit 3)
+        (offset 0))
 
 
 
-;; -- User entity/spec -----------------------------------------------
-(def user-table 
-  (def-glu-table "glu_user" [{:name "user_id" :type "int" :null false :primary true :auto_increment true}
-                             {:name "name" :type "varchar(255)" :null false}
-                             {:name "create_date" :type "datetime" :null false}
-                             {:name "update_date" :type "datetime" :null false}
-                             {:name "active" :type "boolean" :default "1"}]))
-
-(create-table user-table)
--- src/glurps/process/db/db.clj
+++ src/glurps/process/db/db.clj
@@ -1,42 +1,93 @@
 (ns glurps.process.db.db
-  (:require [clojure.java.jdbc :as jdbc]
-            [glurps.config :as config]
-            [wlh.logger :as logger]))
+  (:use korma.db
+        korma.core)
+  (:require [clojure.java.jdbc :as j]
+            [clojure.spec.alpha :as s]))
 
-;; (defn get-clause-from-params [params & clauses]
-;;   (conj {}
-;;         (when clauses (first clauses))
-;;         (when (:order params)
-;;           {:order-by [[(keyword (:order params))
-;;                        (if (= (:asc params) "1")
-;;                          :asc :desc)]]})))
+;; Define the db
+(def db (mysql {:host "localhost"
+                :db "glurps"
+                :user "root"
+                :password "root"}))
 
-;; (defn get-sql-map-from-params [table-name params & clauses]
-;;   "Takes a `params` map and returns a honeysql sql-map.
-;;   `params` represents the page parameters. e.g POST, GET, parameters."
-;;   (conj {}
-;;         {:select [:*]}
-;;         {:from [(keyword table-name)]}
-;;         (get-clause-from-params params (first clauses))))
+(defdb glurps db)
 
-;; (defmacro query-old [db sql-params]
-;;   "Do a query with jdbc, log the request"
-;;   `(let [query# ~sql-params]
-;;      (logger/info (str "[db] " query#))
-;;      (jdbc/query ~db query#)))
+(defn- merge-vector-of-map [v1 v2]
+  "Merge two vectors of map.
+  Note: this is bad implemented but i don't know how
+  do better for now."
+  (let [cnt (count v2)
+        result (atom [])]
+    (dotimes [i cnt]
+      (let [m1 (when (< i (count v1)) (nth v1 i))
+            m2 (nth v2 i)]
+        (swap! result conj (merge m1 m2))))
+    @result))
 
-;; (defn insert [db table cols row]
-;;   (let [rows (into [] (map (fn [col]
-;;                              (row (keyword col))) cols))]
-;;     (jdbc/insert! db table cols rows)))
+(defn- map-column-to-jdbc-column [col]
+  (into []
+        (flatten
+         (map (fn [a]
+                (let [k (first a)
+                      v (first (rest a))]
+                  (conj []
+                        (case k
+                          :name [(keyword v)]
+                          :type [v]
+                          :null (if-not v [:not :null])
+                          :primary (when v [:primary :key])
+                          :auto_increment (when v [:auto_increment])
+                          :default (when v [:default v])
+                          nil)))) col))))
 
-;; (defn update! [db table set-map where-clause]
-;;   (jdbc/update! db table set-map where-clause))
+(defn- map-columns-to-entity-fields [glu-table]
+  (map #(keyword (:name %)) (:columns glu-table)))
 
-;; (defn delete [db table-name id]
-;;   (jdbc/delete! db table-name [(str "id = " id)]))
+(defn- params-to-korma [params]
+  (let [where-clause (list 'where {:fav "on"})
+        order (when (:order params)
+                (reverse (conj '()
+                               'order
+                               (keyword (:order params))
+                               (keyword (if (= (:asc params) "1")
+                                          :ASC :DESC)))))
+        limit (when (:limit params)
+                (reverse (conj () 'limit (:limit params))))
+        offset (when (:offset params)
+                 (reverse (conj () 'offset (:offset params))))]
+    (reverse (remove #(= nil %) 
+                     (conj '()
+                           where-clause
+                           order
+                           limit
+                           offset)))))
+(defn create-table [db table]
+  (let [name (:name table)
+        columns (:jdbc-columns table)]
+    (j/db-do-commands db 
+                      (clojure.string/lower-case 
+                       (j/create-table-ddl name columns
+                                           {:table-spec "ENGINE=InnoDB"
+                                            :entities clojure.string/upper-case})))))
 
-;; (defn query [db sql-map offset limit]
-;;   (let [query (sql/format (sql/build sql-map :offset offset :limit limit))]
-;;     (logger/info (str "[db]" query))
-;;     (jdbc/query db query)))
+(defn drop-table [db name]
+  (j/db-do-commands db (j/drop-table-ddl name)))
+
+(defn def-glu-table [name columns & {:keys [extend]}]
+  (let [abstract-columns (when extend (:columns extend))
+        abstract-jdbc-columns (when abstract-columns
+                                (map map-column-to-jdbc-column abstract-columns))
+        columns (merge-vector-of-map abstract-columns columns)
+        target-jdbc-columns (map map-column-to-jdbc-column columns)        
+        jdbc-columns (into []
+                           (if abstract-jdbc-columns 
+                             (apply merge abstract-jdbc-columns target-jdbc-columns)
+                             target-jdbc-columns))] 
+    
+    {:name name
+     :columns columns
+     :jdbc-columns jdbc-columns
+     :fields (into [] (map #(first %) jdbc-columns))}))
+
+(defmacro select-from-params [table query]
+  `(select ~table ~@(params-to-korma query)))
